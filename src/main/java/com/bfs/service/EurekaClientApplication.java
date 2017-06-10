dabef35f36510da9750dec15aa36bdc7f018f828
mirudhayaraj <mirudhayaraj@paypal.com>
2017-06-10 19:31:14 +0530

    code clenaup and added for travel details

--- src/main/java/com/bfs/service/EurekaClientApplication.java
+++ src/main/java/com/bfs/service/EurekaClientApplication.java
@@ -1,9 +1,8 @@
 package com.bfs.service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,37 +12,48 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 @EnableDiscoveryClient
 public class EurekaClientApplication {
 
-    public static final String APP_NAME = "CITYSEARCH";
+    public static final String CITY_SEARCH = "CITYSEARCH";
+    
+    public static final String TRAVEL_DETAILS = "TRAVELDETAILS";
 
     @Autowired
     private DiscoveryClient discoveryClient;
-
-    private RestTemplate restTemplate = new RestTemplate();
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
 
     @RequestMapping(value = "/getCities", method = RequestMethod.GET)
-    public Map<String,String> getCities() {
-        Map<String, String> result = null;
-        List<ServiceInstance> serviceInstances= this.discoveryClient.getInstances(APP_NAME);
+    public List<Object> getCities() {
+        List<Object> result = new ArrayList<Object>();
+        List<ServiceInstance> serviceInstances= this.discoveryClient.getInstances(CITY_SEARCH);
         String restURI = null;
         for (ServiceInstance ServiceInstance : serviceInstances) {
-            if (ServiceInstance.getServiceId().equals(APP_NAME)) {
-                restURI = ServiceInstance.getUri().toString() + "/" + "citySearch";
+            if (ServiceInstance.getServiceId().equals(CITY_SEARCH)) {
+                restURI = ServiceInstance.getUri().toString() + "/" + "v1/trip/city-search";
             }
         }
-        if (StringUtils.isNotEmpty(restURI)) {
-            result = restTemplate.getForObject(restURI, Map.class);
+        result.add(restURI);
+        return result;
+    }
+
+    @RequestMapping(value = "travel-details", method = RequestMethod.GET)
+    public List<Object> getTravelDetails() {
+        List<Object> result = new ArrayList<Object>();
+        List<ServiceInstance> serviceInstances= this.discoveryClient.getInstances(TRAVEL_DETAILS);
+        String restURI = null;
+        for (ServiceInstance ServiceInstance : serviceInstances) {
+            if (ServiceInstance.getServiceId().equals(TRAVEL_DETAILS)) {
+                restURI = ServiceInstance.getUri().toString() + "/" + "v1/trip/travel-details";
+            }
         }
+        result.add(restURI);
         return result;
     }
 }
